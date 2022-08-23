b48dffb42f361f23950d6396f5dda222ae258fa9
alekspanteli <alekspanteli@rambler.ru>
2022-08-23 14:09:59 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as css from "./scss/index.module.scss"
 
 const user = {
     name: 'Hedy Lamarr',
-  imageUrl: './images/user-profile.webp',
+  imageUrl: "./user-profile.webp",
   imageSize: 90,
 }
 
@@ -27,7 +27,7 @@ const App = () => {
           height: user.imageSize
         }}
       /> */}
-      <img src="./images/user.png" />
+      <img src="https://codesandbox.io/11c364a1-e9f9-439f-97bb-8a7717f751eb" />
       <MyButton />
     </div>
   );
