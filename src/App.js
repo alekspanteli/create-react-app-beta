b7b3c3d3b6f4e0593662b986a4b4067327e0efa8
Alex Panteli <alekspanteli@gmail.com>
2022-08-23 22:04:48 +0300

    fix image and update layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import * as css from "./scss/index.module.scss"
+// import * as css from "./scss/index.module.scss";
+import "./scss/index.scss";
+import UserName from "./images/user.png";
 
 const user = {
-    name: 'Hedy Lamarr',
-  imageUrl: "./user-profile.webp",
+  name: "Hedy Lamarr",
+  imageUrl: UserName,
   imageSize: 90,
-}
+};
 
-const MyButton = () =>{
-    return (
-    <button className={css.button}>I'm a button</button>
-  );
-}
-
-
+const MyButton = () => {
+  return <button className="button">I'm a button</button>;
+};
 
 const App = () => {
   return (
-    <div className={css.app}>
+    <div className="container">
       <h1>{user.name}</h1>
-      {/* <img
+      <img
         className="avatar"
         src={user.imageUrl}
-        alt={'Photo of ' + user.name}
+        alt={"Photo of " + user.name}
         style={{
           width: user.imageSize,
-          height: user.imageSize
+          height: user.imageSize,
         }}
-      /> */}
-      <img src="https://codesandbox.io/11c364a1-e9f9-439f-97bb-8a7717f751eb" />
+      />
       <MyButton />
     </div>
   );
-}
+};
 
 export default App;
